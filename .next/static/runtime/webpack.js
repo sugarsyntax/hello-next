7f015516a82dd4a2077f5cbb03aa0981ad861190
sugarsyntax <jacquelline500@gmail.com>
2018-10-17 16:18:25 -0700

    Add about.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "52c3190ee9a9501aa176";
+/******/ 	var hotCurrentHash = "1897223c5fa6194ccef7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
