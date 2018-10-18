a49f98d191c5531a384dc99b869466d48a8dd201
sugarsyntax <jacquelline500@gmail.com>
2018-10-17 20:39:41 -0700

    Add header to new pages

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import Link from 'next/link' 
+import Header from '../components/Header'   
+
+   const Index = () => (
+        <div>
+            <Header />
+            <p>Contact Page</p>
+        </div>
+    )
---
+++ pages/portfolio.js
@@ -0,0 +1,9 @@
+import Link from 'next/link' 
+import Header from '../components/Header'   
+
+   const Index = () => (
+        <div>
+            <Header />
+            <p>Portfolio Page</p>
+        </div>
+    )
